17d901c9b301b8a4b3fdf29e4fba23b4ba596c5e
Rodchenko Nikita <bira10a@gmail.com>
2024-02-29 00:27:54 +0300

    added section aboutUs

--- app/js/main.js
+++ app/js/main.js
@@ -10,4 +10,17 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
     header();
+
+    // const test = () => {
+    //   const header = document.querySelector('.header');
+  
+    //   document.addEventListener('scroll', () => {
+    //     if (window.pageYOffset >= 200) {
+    //       header.classList.add('header--active');
+    //     } else {
+    //       header.classList.remove('header--active');
+    //     }   
+    //   })
+    // };
+    // test();
 });

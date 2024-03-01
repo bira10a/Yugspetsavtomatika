3804ab612bdd2f32f40616d9f62baf1e003b4b2e
bira10a <94951002+bira10a@users.noreply.github.com>
2024-03-01 13:17:03 +0300

    added burger button and changed header

--- app/js/main.js
+++ app/js/main.js
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     header();
 
-    // const test = () => {
-    //   const header = document.querySelector('.header');
+    const test = () => {
+      const header = document.querySelector('.header');
   
     //   document.addEventListener('scroll', () => {
     //     if (window.pageYOffset >= 200) {
@@ -21,6 +21,31 @@ document.addEventListener('DOMContentLoaded', () => {
     //       header.classList.remove('header--active');
     //     }   
     //   })
-    // };
-    // test();
+
+    window.addEventListener('scroll', () => {
+        if (document.documentElement.scrollTop > 100) {
+          console.log(document.documentElement.scrollTop);
+        } else {
+            console.log(document.documentElement.scrollTop);
+        }
+      });
+
+    };
+    test();
+
+    const burgerMenu = () => {
+      const burger = document.querySelector('.burger'),
+            body = document.querySelector('body');
+
+      burger.addEventListener('click', () => {
+        burger.classList.toggle('burger--active');
+
+        if(burger.classList.contains('burger--active')) {
+          body.classList.add('body--lock');
+        } else {
+          body.classList.remove('body--lock');
+        }
+      });
+    };
+    burgerMenu();
 });

7b67e553628e2bd7b823c7d91e737a1fe684dc22
Rodchenko Nikita <bira10a@gmail.com>
2024-03-20 22:20:48 +0300

    added home

--- app/js/main.js
+++ app/js/main.js
@@ -40,4 +40,150 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     };
     burgerMenu();
+
+
+    const myAnimation = () => {
+      const block = document.querySelectorAll('.header__list li');
+      const block2 = document.querySelectorAll('.header-aside__item');
+      const logo = document.querySelector('.header__logo');
+      
+      setTimeout(() => {
+        logo.style.transform = 'translateY(0)';
+        logo.style.opacity = 1;
+      }, 1100);
+
+      setTimeout(() => {
+        block.forEach(i => {
+          i.style.transform = 'translateY(0)';
+          i.style.opacity = 1;
+        });
+      }, 1300);
+
+      setTimeout(() => {
+        block2.forEach(i => {
+          i.style.transform = 'translateX(0)';
+          i.style.opacity = 1;
+        });
+      }, 1700);
+    }
+    myAnimation();
+
+    const modalInfoAbout = () => {
+      const button = document.querySelectorAll('.aboutUs__item-btn'),
+            modal = document.querySelector('.modal'),
+            modalClose = document.querySelector('.modal__close');
+
+      button.forEach((btn, num) => {
+        btn.addEventListener('click', () => {
+          modal.classList.add('modal--active');
+        });
+      });
+
+      modalClose.addEventListener('click', () => {
+        modal.classList.remove('modal--active');
+      });
+
+      document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('modal--active')) {
+          modal.classList.remove('modal--active');
+        }
+      });
+
+      modal.addEventListener('click', (e) => {
+        if (!e.target.closest('.modal__content')) {
+          if (modal.classList.contains('modal--active')) {
+            modal.classList.remove('modal--active');
+          }
+        }
+      });
+
+      //modal
+    };
+    modalInfoAbout();
+  ///////////////////////////////////////// GSAP
+
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+  if (ScrollTrigger.isTouch !== 1) {
+
+    ScrollSmoother.create({
+      wrapper: '.wrapper',
+      content: '.content',
+      smooth: 1.5,
+      effects: true
+    })
+
+    let itemsL = gsap.utils.toArray('.aboutUs__title')
+
+    itemsL.forEach(item => {
+      gsap.fromTo(item, { opacity: 0, x: -200 }, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: '-650',
+          end: '-100',
+          scrub: true
+        }
+      })
+    })
+    
+    let itemsR = gsap.utils.toArray('.aboutUs__text')
+
+    itemsR.forEach(item => {
+      gsap.fromTo(item, { opacity: 0, x: +200 }, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: '-650',
+          end: '-100',
+          scrub: true
+        }
+      })
+    })
+
+    let aboutUsItemLeft = gsap.utils.toArray('.aboutUs__item--left')
+
+    aboutUsItemLeft.forEach(item => {
+      gsap.fromTo(item, { opacity: 0, x: -500 }, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: '-550',
+          end: '-150',
+          scrub: true
+        }
+      })
+    })
+
+    let aboutUsItemRight = gsap.utils.toArray('.aboutUs__item--right')
+
+    aboutUsItemRight.forEach(item => {
+      gsap.fromTo(item, { opacity: 0, x: +500 }, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: '-550',
+          end: '-150',
+          scrub: true
+        }
+      })
+    })
+
+    // gsap.fromTo('.header', { opacity: 1 }, {
+    //   opacity: 0,
+    //   scrollTrigger: {
+    //     trigger: '.header',
+    //     start: 'center',
+    //     end: '650',
+    //     scrub: true
+    //   }
+    // })
+
+    // end if
+  }
+
 });
+
+// window.onload = () => {
+//   console.log(`good`);
+// }

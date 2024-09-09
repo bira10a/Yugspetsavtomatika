45a0c16faa315e4fd5d15eaa4cb08ccaf0bd58dc
bira10a <94951002+bira10a@users.noreply.github.com>
2024-09-09 12:02:20 +0300

    job wor old, 09.09.2024

--- app/js/main.js
+++ app/js/main.js
@@ -109,6 +109,7 @@ document.addEventListener('DOMContentLoaded', () => {
   tlLoader
   .set('.loader__item', {yPercent: -100})
   .set('.loader__title', {opacity: 0})
+  .set('.loader__logo', {opacity: 0})
   .to('.loader__item', {
     yPercent: 0,
     duration: 0.5,
@@ -119,12 +120,37 @@ document.addEventListener('DOMContentLoaded', () => {
     duration: 0.5,
     stagger: 0.25,
   })
+  .to('.loader__logo', {
+    opacity: 1,
+    duration: 1,
+    scale: 1.2,
+  })
   .to('.loader__title', {
     opacity: 1,
     duration: 1,
     scale: 1.2,
   })
-  
+  .set('.loader__item', {
+    yPercent: -100,
+  })
+  .to('.loader__logo', {
+    opacity: 0,
+    duration: 1,
+    scale: 0.8,
+  })
+  .to('.loader__title', {
+    opacity: 0,
+    duration: 1,
+    scale: 0.8,
+  })
+  .to(
+    '.loader', {
+      yPercent: -100,
+      duration: 1,
+    },
+    '-=0.2'
+  )
+
 
   if (ScrollTrigger.isTouch !== 1) {
 
